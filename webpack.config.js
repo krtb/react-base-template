8a01dd98e64991f2af26f3731169908c425838e2
Kurt <kurt.bauer@pendo.io>
2021-11-16 18:37:43 -0500

    add mini-css-extract-plugin, and HotModuleReloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let mode = 'development';
 
 // set to 'production' via node script
@@ -10,6 +11,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
                 // regex
                 test: /\.js$/i,
                 // memory expensive to runa against all node files
@@ -21,12 +26,18 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [new MiniCssExtractPlugin()],
+
     // Normalizes webpack output file, "main.js", for inspection
     // devtool: false,
     devtool: 'source-map',
     devServer: {
         // In new Webpack Version, no longer "contentBase" config.
         // Needs relative path
-        static: "./dist"
+        static: "./dist",
+        // can add via script, though this method adds flag, adds module to plugin chain
+        // injects styles, rather than rendering all CSS, which can speed up development
+        hot: true,
     }
 }
