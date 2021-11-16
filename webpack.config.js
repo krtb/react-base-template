e36d0c5b3794c5ed9515b2e85b810234cb2f8512
Kurt <kurt.bauer@pendo.io>
2021-11-16 18:01:00 -0500

    add webpack rules for babel-loader/.babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     mode: 'development',
+    module: {
+        rules: [
+            {
+                // regex
+                test: /\.js$/i,
+                // memory expensive to runa against all node files
+                exclude: /node_modules/,
+                // tell webpack to use .babel.rc file, with configs
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
     // Normalizes webpack output file, "main.js", for inspection
     devtool: false,
     devServer: {
